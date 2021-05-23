1575fddfe33eefcbbf9aac7c435576ea9dfc2d3c
Jiayu Liu <jiayu.liu@airbnb.com>
2021-05-23 22:04:29 +0800

    add first and last value

--- datafusion/src/physical_plan/expressions/nth_value.rs
+++ datafusion/src/physical_plan/expressions/nth_value.rs
@@ -17,17 +17,117 @@
 
 //! Defines physical expressions that can evaluated at runtime during query execution
 
-use crate::error::Result;
+use crate::error::{DataFusionError, Result};
 use crate::physical_plan::{BuiltInWindowFunctionExpr, PhysicalExpr, WindowAccumulator};
 use arrow::array::ArrayRef;
 use arrow::datatypes::{DataType, Field};
 use std::any::Any;
 use std::sync::Arc;
 
+/// first_value expression
+#[derive(Debug)]
+pub struct FirstValue {
+  name: String,
+  data_type: DataType,
+  expr: Arc<dyn PhysicalExpr>,
+}
+
+impl FirstValue {
+  /// Create a new FIRST_VALUE window aggregate function
+  pub fn new(
+    expr: Arc<dyn PhysicalExpr>,
+    name: String,
+    data_type: DataType,
+  ) -> Result<Self> {
+    Self {
+      name,
+      expr,
+      data_type,
+    }
+  }
+}
+
+impl BuiltInWindowFunctionExpr for FirstValue {
+  /// Return a reference to Any that can be used for downcasting
+  fn as_any(&self) -> &dyn Any {
+    self
+  }
+
+  fn field(&self) -> Result<Field> {
+    let nullable = true;
+    Ok(Field::new(&self.name, self.data_type.clone(), nullable))
+  }
+
+  fn expressions(&self) -> Vec<Arc<dyn PhysicalExpr>> {
+    vec![self.expr.clone()]
+  }
+
+  fn name(&self) -> &str {
+    &self.name
+  }
+
+  fn create_accumulator(&self) -> Result<Box<dyn WindowAccumulator>> {
+    Ok(Box::new(NthValueAccumulator::new(1, self.data_type)))
+  }
+}
+
+const SPECIAL_USIZE_VALUE_FOR_LAST = 0usize;
+
+/// last_value expression
+#[derive(Debug)]
+pub struct LastValue {
+  name: String,
+  data_type: DataType,
+  expr: Arc<dyn PhysicalExpr>,
+}
+
+impl LastValue {
+  /// Create a new FIRST_VALUE window aggregate function
+  pub fn new(
+    expr: Arc<dyn PhysicalExpr>,
+    name: String,
+    data_type: DataType,
+  ) -> Result<Self> {
+    Self {
+      name,
+      expr,
+      data_type,
+    }
+  }
+}
+
+impl BuiltInWindowFunctionExpr for LastValue {
+  /// Return a reference to Any that can be used for downcasting
+  fn as_any(&self) -> &dyn Any {
+    self
+  }
+
+  fn field(&self) -> Result<Field> {
+    let nullable = true;
+    Ok(Field::new(&self.name, self.data_type.clone(), nullable))
+  }
+
+  fn expressions(&self) -> Vec<Arc<dyn PhysicalExpr>> {
+    vec![self.expr.clone()]
+  }
+
+  fn name(&self) -> &str {
+    &self.name
+  }
+
+  fn create_accumulator(&self) -> Result<Box<dyn WindowAccumulator>> {
+    Ok(Box::new(NthValueAccumulator::new(
+      SPECIAL_USIZE_VALUE_FOR_LAST,
+      self.data_type,
+    )))
+  }
+}
+
 /// nth_value expression
 #[derive(Debug)]
 pub struct NthValue {
   name: String,
+  n: usize,
   data_type: DataType,
   expr: Arc<dyn PhysicalExpr>,
 }
@@ -37,13 +137,21 @@ impl NthValue {
   pub fn try_new(
     expr: Arc<dyn PhysicalExpr>,
     name: String,
+    n: usize,
     data_type: DataType,
   ) -> Result<Self> {
-    Ok(Self {
-      name,
-      expr,
-      data_type,
-    })
+    if n == SPECIAL_USIZE_VALUE_FOR_LAST {
+      Err(DataFusionError::SQL(
+        "nth_value expect n to be > 0".to_owned(),
+      ))
+    } else {
+      Ok(Self {
+        name,
+        expr,
+        n,
+        data_type,
+      })
+    }
   }
 }
 
@@ -67,12 +175,14 @@ impl BuiltInWindowFunctionExpr for NthValue {
   }
 
   fn create_accumulator(&self) -> Result<Box<dyn WindowAccumulator>> {
-    Ok(Box::new(NthValueAccumulator::new()))
+    Ok(Box::new(NthValueAccumulator::new(self.n, self.data_type)))
   }
 }
 
 #[derive(Debug)]
 struct NthValueAccumulator {
+  // n the target nth_value, however we'll reuse it for last_value acc, so when n == 0 it specifically
+  // means last
   n: usize,
   offset: usize,
   value: ScalarValue,
@@ -92,7 +202,10 @@ impl NthValueAccumulator {
 
 impl WindowAccumulator for NthValueAccumulator {
   fn scan(&mut self, _values: &[ScalarValue]) -> Result<Option<ScalarValue>> {
-    if self.offset < self.n {
+    if self.n == SPECIAL_USIZE_VALUE_FOR_LAST {
+      // for last_value function
+      self.value = Some(&values[0]);
+    } else if self.offset < self.n {
       self.offset += 1;
       if self.offset == self.n {
         self.value = Some(&values[0]);
